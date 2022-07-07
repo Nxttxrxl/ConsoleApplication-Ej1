94f9de3a3cdd87721d6314957cc758e40769ebc9
natt <102251657+Nxttxrxl@users.noreply.github.com>
2022-07-07 13:48:26 +0200

    Add files via upload

---
+++ Prueba.vb
@@ -0,0 +1,12 @@
+﻿Namespace ConsoleApplication5
+{
+    Class Program
+    {
+        Static void Main(String[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); //Para la ejecucion hasta presionar tecla
+        }
+    }
+}
+
